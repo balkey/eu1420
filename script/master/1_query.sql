1e536ee708e5176abbb93f8e8e09632bb12421cc
balkey <balkey80@gmail.com>
2020-07-31 15:42:07 +0100

    feature: LU and LV added to master

--- script/master/1_query.sql
+++ script/master/1_query.sql
@@ -3796,6 +3796,101 @@ SELECT
   FROM raw."2014ITESF_1"
 ),
 
+"2014LU16RFOP001_1" AS (
+  SELECT
+    operation_name AS operation_name,
+    beneficiary_name AS beneficiary_name,
+    operation_summary AS operation_summary,
+    CASE
+      WHEN is_numeric("operation_start_date") THEN to_timestamp_from_excel(TO_NUMBER("operation_start_date", '99999D9')::INTEGER)::DATE
+      ELSE TO_DATE("operation_start_date",'DD.MM.YYYY')::DATE
+    END AS operation_start_date,
+    CASE
+      WHEN is_numeric("operation_end_date") THEN to_timestamp_from_excel(TO_NUMBER("operation_end_date", '99999D9')::INTEGER)::DATE
+      ELSE TO_DATE("operation_end_date",'DD.MM.YYYY')::DATE
+    END AS operation_end_date,
+    'EUR' AS currency,
+    total_eligible_expenditure::DECIMAL AS operation_total_expenditure,
+    erdf_cofinancing_40::DECIMAL / total_eligible_expenditure::DECIMAL*100.0 AS eu_cofinancing_rate,
+    NULL AS country,
+    municipality AS operation_location,
+    category_of_intervention AS code_of_category_intervention,
+    NULL AS name_of_category_intervention,
+    NULL::DATE AS date_of_last_update,
+    'LU' AS operation_nuts0,
+    NULL AS operation_nuts1_code,
+    NULL AS operation_nuts1_name,
+    NULL AS operation_nuts2_code,
+    NULL AS operation_nuts2_name,
+    NULL AS operation_nuts3_code,
+    NULL AS operation_nuts3_name,
+    NULL AS operation_lau1_code,
+    NULL AS operation_lau1_name,
+    NULL AS operation_lau2_code,
+    municipality AS operation_lau2_name,
+    NULL AS operation_city,
+    NULL AS operation_district,
+    NULL AS operation_zip_code,
+    NULL::DECIMAL AS member_state_value,
+    NULL::DECIMAL AS eu_subsidy_value,
+    NULL AS beneficiary_id,
+    ndeg_of_project AS operation_id,
+    axis AS priority_axis,
+    NULL AS form_of_finance,
+    NULL AS territorial_dimension,
+    NULL AS territorial_delivery_mechanism,
+    NULL AS esf_secondary_theme,
+    NULL AS economic_dimension,
+    '2014LU16RFOP001' AS cci,
+    '2014LU16RFOP001_1' AS data_source
+  FROM raw."2014LU16RFOP001_1"
+  WHERE beneficiary_name NOT IN ('Beneficiary name','Nom du bénéficiaire')
+),
+
+"2014LV16MAOP001_1" AS (
+  SELECT
+    projektanosaukums__operation_namenameofproject AS operation_name,
+    iesniedzejanosaukums__beneficiary_namenameofbeneficiary AS beneficiary_name,
+    kopsavilkumssummary AS operation_summary,
+    TO_DATE("sakumadatumsstartdate",'DD.MM.YYYY H:M:SS') AS operation_start_date,
+    TO_DATE(beigudatumsfinishdate,'DD.MM.YYYY H:M:SS') AS operation_end_date,
+    'EUR' AS currency,
+    kopejaisfinansejumstotalfinancing::DECIMAL AS operation_total_expenditure,
+    (esfondulidzfinansejumsunioncofinancing::DECIMAL / kopejaisfinansejumstotalfinancing::DECIMAL)*100.0 AS eu_cofinancing_rate,
+    NULL AS country,
+    istenosanasvietasregionsregion AS operation_location,
+    intervenceskategorijaskodscategoryofinterventioncode AS code_of_category_intervention,
+    intervenceskategorijasnosaukumscategoryofinterventionname AS name_of_category_intervention,
+    TO_DATE(pedejasdatuatjauninasanasdatumsdateoflastupdate, 'DD.MM.YYYY H:M:SS') AS date_of_last_update,
+    'LV' AS operation_nuts0,
+    NULL AS operation_nuts1_code,
+    NULL AS operation_nuts1_name,
+    NULL AS operation_nuts2_code,
+    NULL AS operation_nuts2_name,
+    NULL AS operation_nuts3_code,
+    istenosanasvietasregionsregion AS operation_nuts3_name,
+    NULL AS operation_lau1_code,
+    NULL AS operation_lau1_name,
+    NULL AS operation_lau2_code,
+    NULL AS operation_lau2_name,
+    NULL AS operation_city,
+    NULL AS operation_district,
+    NULL AS operation_zip_code,
+    NULL::DECIMAL AS member_state_value,
+    esfondulidzfinansejumsunioncofinancing::DECIMAL AS eu_subsidy_value,
+    iesniedzejaregnobeneficiaryregno AS beneficiary_id,
+    projektanumurs__operation_noprojectno AS operation_id,
+    samnosaukumsnameofspecificobjective AS priority_axis,
+    NULL AS form_of_finance,
+    NULL AS territorial_dimension,
+    NULL AS territorial_delivery_mechanism,
+    NULL AS esf_secondary_theme,
+    NULL AS economic_dimension,
+    '2014LV16MAOP001' AS cci,
+    '2014LV16MAOP001_1' AS data_source
+  FROM raw."2014LV16MAOP001_1"
+),
+
 vw AS (
   SELECT * FROM "2014AT16RFOP001"
   UNION ALL
@@ -3942,6 +4037,10 @@ vw AS (
   SELECT * FROM "2014ITERDF_1"
   UNION ALL
   SELECT * FROM "2014ITESF_1"
+  UNION ALL
+  SELECT * FROM "2014LU16RFOP001_1"
+  UNION ALL
+  SELECT * FROM "2014LV16MAOP001_1"
 )
 
 SELECT
