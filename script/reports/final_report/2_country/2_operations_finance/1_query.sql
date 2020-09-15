9c29d65d8ab62042131833cd76325e0105f15fa0
balkey <balkey80@gmail.com>
2020-09-15 10:54:08 +0100

    fix: final report financial data values in integers

--- script/reports/final_report/2_country/2_operations_finance/1_query.sql
+++ script/reports/final_report/2_country/2_operations_finance/1_query.sql
@@ -3,8 +3,8 @@ SELECT
   COUNT(*) AS transaction_count,
   COUNT(DISTINCT operation_name) AS operation_count,
   COUNT(DISTINCT beneficiary_name) AS beneficiary_count,
-  SUM(operation_total_expenditure_eur) AS operation_total_expenditure,
-  ROUND(SUM(operation_total_expenditure_eur/100.0*COALESCE(eu_cofinancing_rate,1.0)),2) AS subsidies_allocated,
+  ROUND(SUM(operation_total_expenditure_eur),0) AS operation_total_expenditure,
+  ROUND(SUM(operation_total_expenditure_eur/100.0*COALESCE(eu_cofinancing_rate,1.0)),0) AS subsidies_allocated,
   ROUND(AVG(eu_cofinancing_rate),2) AS avg_eu_cofinancing_rate
 FROM real.operations_eu
 GROUP BY 1
